1dd01db0a2294d8ff664eec208ec43ea9f185915
Valeriya Izotova <my@valerka-made.ru>
2023-07-01 01:34:41 +0300

    тест

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -90,7 +90,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'None',
-        secure: true,
+        // secure: true,
       })
         .send({ message: 'Авторизация прошла успешно' });
     })
