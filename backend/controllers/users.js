6c080d8e83c0bc493e0d0dc9b664ef586a2cc9eb
Valeriya Izotova <my@valerka-made.ru>
2023-07-01 06:51:15 +0300

    sameSite: 'None'

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -89,7 +89,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('token', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'Lax',
+        sameSite: 'None',
         path: '/',
         secure: true,
       })
